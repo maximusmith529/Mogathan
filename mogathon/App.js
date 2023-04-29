b5726ea6b7c2f5b53a6b7b3be6120be33bd03f71
Bryson Paul <brysonpaul1870@gmail.com>
2023-04-29 04:55:29 -0400

    adds text finding off of the image inputted by the textpicker

--- mogathon/App.js
+++ mogathon/App.js
@@ -1,10 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+import Constants from 'expo-constants';
 
 export default function App() {
+
+  const fetchVision = async () => {
+
+    let result = await  ImagePicker.launchImageLibraryAsync({
+      mediaTypes:await ImagePicker.MediaTypeOptions.All,
+      allowsEditing:true,
+      //aspect:[4,3],
+      quality:0.5
+  
+  })
+   
+  const base64 = await FileSystem.readAsStringAsync(result.assets[0].uri, { encoding: 'base64' });
+    console.log(Constants?.manifest?.extra?.apiKey);
+    await fetch('https://vision.googleapis.com/v1/images:annotate?key='+Constants?.manifest?.extra?.apiKey, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        requests: [
+          {
+            image: {
+              content: base64
+            },
+            features: [
+              {
+                type: "TEXT_DETECTION"
+              }
+            ]
+          }
+        ]
+      })
+    }).then((response) => response.json()).then((json) => {
+      console.log(json.responses[0].fullTextAnnotation.text);
+  });
+  }
+  fetchVision();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>testing123</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ mogathon/app.config.js
@@ -0,0 +1,38 @@
+
+import 'dotenv/config';
+
+export default{
+  expo: {
+    name: "mogathon",
+    slug: "mogathon",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
+    },
+      web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      // Add your extra configs here
+      apiKey: process.env.REACT_NATIVE_VISION_API_KEY
+    }
+
+  }
+}
