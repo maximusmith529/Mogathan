ea66c9cd00e3c44d6e6511ad065f165cbd10c232
maximusmith529 <maximusmith529@gmail.com>
2023-04-29 18:30:03 -0400

    Updated Summary Page
    Added bubbles and vanishingKeyboard

--- mogathon/pages/SummaryPage.js
+++ mogathon/pages/SummaryPage.js
@@ -1,72 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useRef, useState } from 'react';
-import {
-    StyleSheet,
-    Text,
-    View,
-    ScrollView,
-    Image,
-    TextInput,
-    Pressable,
-    TouchableOpacity,
-    ImageBackground,
-    Dimensions,
-    Animated,
-} from 'react-native';
+import React, { useRef, useState, useEffect } from 'react';
+import
+    {
+        StyleSheet,
+        Text,
+        View,
+        ScrollView,
+        Image,
+        TextInput,
+        Pressable,
+        TouchableOpacity,
+        ImageBackground,
+        Dimensions,
+        Animated,
+        KeyboardAvoidingView,
+        Platform,
+    } from 'react-native';
 // SummaryPage.js
 
-const SummaryPage = ({route, navigation, initialPrompt }) => {
-  const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState('');
+const SummaryPage = ({ route, navigation }) =>
+{
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
+    let summary = route.params?.summary;
+    useEffect(() =>
+    {
+        if (summary)
+        {
+            setMessages([{ text: summary, sender: 'bot' }]);
+        }
+    }, [summary]);
 
-  const handleSend = () => {
-    if (newMessage.trim() !== '') {
-      setMessages([...messages, { text: newMessage, sender: 'user' }]);
-      setNewMessage('');
-    }
-  };
-
-  return (
-   
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={{ fontWeight: 'bold' }}>Chat with Support</Text>
-        </View>
-        <View style={styles.body}>
-          <Text style={{ fontStyle: 'italic' }}>{initialPrompt}</Text>
-        </View>
-        <View style={{ flex: 1, padding: 10 }}>
-          {messages.map((message, index) => (
-            <View
-              key={index}
-              style={{
-                alignSelf: message.sender === 'user' ? 'flex-end' : 'flex-start',
-                backgroundColor: message.sender === 'user' ? '#2196f3' : '#f5f5f5',
-                padding: 10,
-                borderRadius: 5,
-                marginBottom: 10,
-              }}
-            >
-              <Text style={{ color: message.sender === 'user' ? '#fff' : '#000' }}>{message.text}</Text>
+    const handleSend = () =>
+    {
+        if (newMessage.trim() !== '')
+        {
+            setMessages([...messages, { text: newMessage, sender: 'user' }]);
+            setNewMessage('');
+        }
+    };
+    const renderMessage = (message, index) =>
+    {
+        const isUser = message.sender === 'user';
+        const messageStyle = isUser ? styles.userMessage : styles.botMessage;
+        const textStyle = isUser ? styles.userMessageText : styles.botMessageText;
+        return (
+            <View key={index} style={[styles.messageContainer, { alignSelf: isUser ? 'flex-end' : 'flex-start' }]}>
+                <View style={[styles.messageBubble, messageStyle]}>
+                    <Text style={textStyle}>{message.text}</Text>
+                </View>
             </View>
-          ))}
-        </View>
-        <View style={{ flexDirection: 'row', padding: 10 }}>
-          <TextInput
-            style={{ flex: 1, marginRight: 10, borderRadius: 5, borderWidth: 1, borderColor: '#ccc', padding: 10 }}
-            placeholder="Type a message"
-            value={newMessage}
-            onChangeText={(text) => setNewMessage(text)}
-          />
-          <TouchableOpacity
-            style={{ backgroundColor: '#2196f3', padding: 10, borderRadius: 5 }}
-            onPress={handleSend}
-          >
-            <Text style={{ color: '#fff', fontWeight: 'bold' }}>Send</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-  );
+        );
+    };
+
+    return (
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 25}
+        >
+            <View style={styles.header}>
+                <TouchableOpacity style={styles.backBtnContainer} onPress={() => { navigation.navigate('HomePage') }}>
+                    <Image style={styles.backBtnImage} source={require('../assets/back-btn.png')} />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.chatContainer}>
+                {messages.map(renderMessage)}
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Type a message"
+                    value={newMessage}
+                    onChangeText={(text) => setNewMessage(text)}
+                />
+                <TouchableOpacity style={styles.sendButton} onPress={handleSend}>
+                    <Text style={styles.sendButtonText}>Send</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
+    );
 };
 
 export default SummaryPage;
@@ -90,48 +103,69 @@ export default SummaryPage;
 //     )
 // }
 
+
 const styles = StyleSheet.create({
-	container: {
-        display: 'flex',
-        flexDirection: 'column',
+    container: {
         flex: 1,
-        width: '100%',
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'grey'
+        backgroundColor: '#e0f2fe',
     },
-	header: {
-        height: '10%',
-        width: '100%',
-        justifyContent: 'center',
-        alignItems: 'flex-start'
+    chatContainer: {
+        flex: 1,
+        paddingHorizontal: 10,
     },
-	backBtnContainer: {
-        paddingLeft: 25
+    messageContainer: {
+        marginVertical: 5,
     },
-	backBtnImage: {
-        width: 40,
-        height: 40
+    messageBubble: {
+        borderRadius: 20,
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        maxWidth: '80%',
     },
-	body: {
-        display: 'flex',
+    userMessage: {
+        backgroundColor: '#2196F3',
+        alignSelf: 'flex-end',
+    },
+    botMessage: {
+        backgroundColor: '#F5F5F5',
+        alignSelf: 'flex-start',
+    },
+    userMessageText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+    },
+    botMessageText: {
+        color: '#000000',
+        fontSize: 16,
+    },
+    inputContainer: {
         flexDirection: 'row',
-        height: '80%',
-        width: '100%',
         alignItems: 'center',
-        justifyContent: 'space-evenly',
-        paddingBottom: 75,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        
     },
-    testing: {
-        width: '100%',
-        height: '100%'
+    textInput: {
+        flex: 1,
+        height: 40,
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: 'white',
+        backgroundColor: 'white',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        marginRight: 10,
     },
-	scrollView: {
-		backGroundColor: 'white',
-		marginHorizontal: 20
-	},
-	text: {
-		fontSize:18
-	}
+    sendButton: {
+        backgroundColor: '#2196F3',
+        borderRadius: 20,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+    },
+    sendButtonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
 });
+
