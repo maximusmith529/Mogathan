7aec3edc6de4b806e3c25d8a35c6691b20761b5f
Jinan Patel <pateljinan@gmail.com>
2023-04-29 17:19:57 -0400

    fixed help page bugs

--- mogathon/pages/HelpPage.js
+++ mogathon/pages/HelpPage.js
@@ -17,44 +17,44 @@ import {
 export default function HelpPage({ route, navigation }) {
     return (
         <View style={styles.container}>
-			<View style={styles.header}>
+            <View style={styles.header}>
                 <TouchableOpacity style={styles.backBtnContainer} onPress={() => { navigation.navigate('HomePage') }}>
                     <Image style={styles.backBtnImage} source={require('../assets/back-btn.png')} />
                 </TouchableOpacity>
             </View>
 
-			<View style={styles.body}>
-				<ScrollView style={styles.scrollView}>
-					<Text style={styles.question}>
-						What is Morgan & Morgan Mate? {'\n'}
-					</Text>
-					<Text style={styles.answer}>
-						{'\t'} Morgan & Morgan Mate is a legal documentation assistant application 
-						made to assist the average person to make sense of complicated and wordy 
-						legal documents. {'\n'}
-					</Text>
-					<Text style={styles.question}>
-						{'\n'}How does Morgan & Morgan Mate Work?{'\n'}
-					</Text>
-					<Text style={styles.answer}>
-						{'\t'} Morgan & Morgan Mate uses the latest developments in cutting edge AI 
-						technologies to read the provided legal document images and produce a well 
-						crafted synopsis and explanation of the contains within said document. {'\n'}
-					</Text>
-					<Text style={styles.question}>
-						{'\n'} How do I use Morgan and Morgan Mate?{'\n'}
-					</Text>
-					<Text style={styles.answer}>
-						{'\t'} All one has to do to use Morgan and Morgan Mate is either upload 
-						existing images from their device or select the option to take new photos. 
-						Once the desired images are uploaded, the application will produce a synopsis 
-						of the legal documents within the provided images. Users will then have the 
-						ability to get more information about their documents or even ask questions 
-						relating to the contents of the document.{'\n'}
-					</Text>
-				</ScrollView>
-			</View>
-	    </View>
+            <View style={styles.body}>
+                <ScrollView style={styles.scrollView}>
+                    <Text style={styles.question}>
+                        What is Morgan & Morgan Mate? {'\n'}
+                    </Text>
+                    <Text style={styles.answer}>
+                        {'\t'} Morgan & Morgan Mate is a legal documentation assistant application
+                        made to assist the average person to make sense of complicated and wordy
+                        legal documents. {'\n'}
+                    </Text>
+                    <Text style={styles.question}>
+                        {'\n'}How does Morgan & Morgan Mate Work?{'\n'}
+                    </Text>
+                    <Text style={styles.answer}>
+                        {'\t'} Morgan & Morgan Mate uses the latest developments in cutting edge AI
+                        technologies to read the provided legal document images and produce a well
+                        crafted synopsis and explanation of the contains within said document. {'\n'}
+                    </Text>
+                    <Text style={styles.question}>
+                        {'\n'} How do I use Morgan and Morgan Mate?{'\n'}
+                    </Text>
+                    <Text style={styles.answer}>
+                        {'\t'} All one has to do to use Morgan and Morgan Mate is either upload
+                        existing images from their device or select the option to take new photos.
+                        Once the desired images are uploaded, the application will produce a synopsis
+                        of the legal documents within the provided images. Users will then have the
+                        ability to get more information about their documents or even ask questions
+                        relating to the contents of the document.{'\n'}
+                    </Text>
+                </ScrollView>
+            </View>
+        </View>
     )
 }
 
@@ -71,56 +71,29 @@ const styles = StyleSheet.create({
     },
     header: {
         flexDirection: 'row',
-        height: 70,
+        height: '5%',
         width: '100%',
-        justifyContent: 'left',
-        top: "2%",
-        alignItems: 'flex-start'
-    },
-    helpContainer: {
-        paddingRight: 25
-    },
-    helpText: {
-        color: 'white',
-        fontWeight: '900',
-        fontSize: 50
+        justifyContent: 'flex-start',
+        alignItems: 'flex-end'
     },
     body: {
-        height: '95%',
+        height: '85%',
         width: '100%',
         alignItems: 'center',
         justifyContent: 'flex-end',
-        paddingBottom: 75,
     },
-    button: {
-        width: '85%',
-        padding: 10,
-        backgroundColor: '#fff',
-        borderRadius: '40',
-        alignItems: 'center',
-        color: 'white',
-        borderRadius: 15,
-        shadowColor: '#000',
-        shadowOffset: { width: -2, height: 4 },
-        shadowOpacity: 0.2,
-        shadowRadius: 3,
+    scrollView: {
+        height: '100%',
+        width: '95%'
     },
-    buttonText: {
-        fontSize: 25,
-        fontWeight: 'bold',
-    },
-    text: {
+    question: {
+        width: '90%',
         fontSize: 24,
         fontWeight: 'bold',
-        color: 'red'
+        color: 'black',
     },
-	question: {
-		fontSize: 24,
-		fontWeight: 'bold',
-		color: 'black',
-	},
-	answer: {
-		fontSize: 20,
-		color: 'black'
-	}
+    answer: {
+        fontSize: 20,
+        color: 'black'
+    }
 });
